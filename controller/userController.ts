c354b466b7f98a6809c56821f313a9b452f7db96
Benito Ernst <benitoernst@MBP-von-Benito.fritz.box>
2022-12-23 12:33:05 +0100

    update nutzerverwaltung

--- controller/userController.ts
+++ controller/userController.ts
@@ -1,25 +1,25 @@
-import User from  '../models/user';
+import User from '../models/user';
 import SecurityMaster from '../models/securityMaster';
 
 
 // Get all User
-async function getAllUser(req, res){
+async function getAllUser(req, res) {
 
     try {
         const data = await User.find().select('name vorname benutzername email rolle');
-        
+
         res.json(data);
     }
     catch (error: any) {
         res.status(500).json({ message: error.message })
     }
-    
+
 }
 
 // Create User
 async function createUser(req, res) {
 
-    if(req.user.role == "admin"){
+    if (req.user.role == "admin") {
 
         const data = new User({
             name: req.body.name,
@@ -29,82 +29,94 @@ async function createUser(req, res) {
             passwort: req.body.passwort,
             rolle: req.body.rolle
         });
-    
+
         try {
             data.passwort = await SecurityMaster.hashPassword(data.passwort);
-    
+
             const dataToSave = await data.save();
-            
-            res.status(200).json("Der User " + dataToSave.benutzername + " wurde erfolgreich angelegt!")
+
+            res.status(200).json("Der User " + dataToSave.benutzername + " wurde erfolgreich angelegt!");
         }
         catch (error: any) {
             res.status(400).json(error.message);
         }
 
-    }else{
+    } else {
         res.status(403).json("Sie haben keine Berechtigung für diese Aktion!");
     }
 }
 
 // Update User
-async function updateUser(req, res){
+async function updateUser(req, res) {
 
-    if(req.user.role == "admin"){
+    if (req.user.role) {
 
         try {
             const username = req.params.username;
+
+            console.log(username);
 
             const updatedData = {
                 name: req.body.name,
                 email: req.body.email,
                 rolle: req.body.rolle
             };
-    
-            const options = { new: true };
-    
-            const result = await User.findOneAndUpdate(
-                username, updatedData, options
-            )
 
-            if(username == req.params.username){
-                const accessToken = await SecurityMaster.updateToken(username);
-                res.clearCookie('auth');
-                res.cookie('auth', accessToken);
+
+            if (username == req.user.username && updatedData.rolle != req.user.role) {
+
+                res.status(400).json("Sie können Ihre eigene Rolle nicht ändern!");
+
+            } else {
+                const options = { new: true };
+
+                const result = await User.findOneAndUpdate(
+                    { benutzername: username }, updatedData, options
+                )
+
+                console.log(result);
+
+                if (username == req.user.role) {
+                    const accessToken = await SecurityMaster.updateToken(username);
+                    res.clearCookie('auth');
+                    res.cookie('auth', accessToken);
+                }
+
+                res.status(200).json(`Die Änderungen wurden erfolgreich gespeichert!`);
             }
-    
-            res.status(200).json(`Die Änderungen wurden erfolgreich gespeichert!`);
+
         }
         catch (error: any) {
             res.status(400).json(error.message);
         }
 
-    }else{
+    } else {
         res.status(403).json("Sie haben keine Berechtigung für diese Aktion!");
     }
 }
 
 // Delete User
-async function deleteUser(req, res){
+async function deleteUser(req, res) {
 
-    if(req.user.role == "admin"){
+    if (req.user.role == "admin") {
         try {
 
             const username = req.params.username;
-    
-            if(req.user.username != username){
+
+            if (req.user.username != username) {
                 const data = await User.findOneAndDelete({ benutzername: username }).exec();
                 res.status(200).json(`User "${data?.benutzername}" wurde gelöscht.`);
             }
-            else{
+            else {
                 res.status(400).json("Sie können sich nicht selber löschen!");
             }
-            
+
         }
         catch (error: any) {
             res.status(400).json({ message: error.message });
         }
 
-    }else{
+    } else {
         res.status(403).json("Sie haben keine Berechtigung für diese Aktion!");
     }
 }
